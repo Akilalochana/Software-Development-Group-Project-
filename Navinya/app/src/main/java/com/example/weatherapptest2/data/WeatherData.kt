cc2a7977bdf2e4e2d9891b8998276a4ebd8d5e82
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-02-10 12:00:30 +0530

    remove another all gradle projects in main

--- Navinya/app/src/main/java/com/example/weatherapptest2/data/WeatherData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weatherapptest2.data
-
-sealed class WeatherData
-
-data class CurrentLocation(
-    val date: String,
-    val location: String = "Choose your location"
-) : WeatherData()
--- Navinya/app/src/main/java/com/example/weatherapptest2/fragments/location/LocationFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weatherapptest2.fragments.location
-
-import androidx.fragment.app.Fragment
-
-class LocationFragment : Fragment() {
-}
